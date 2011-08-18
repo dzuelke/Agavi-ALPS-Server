8df243e8a8f5e24ee970fdc74486e5849e72cf3e
David Zuelke <david.zuelke@bitextender.com>
2011-08-18 14:58:32 -0400

    add agavi-trunk export from SVN

---
+++ libs/agavi/translation/data/timezones/Africa_47_Maseru.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Data file for timezone "Africa/Maseru".
+ * Compiled from olson file "africa", version 8.31.
+ *
+ * @package    agavi
+ * @subpackage translation
+ *
+ * @copyright  Authors
+ * @copyright  The Agavi Project
+ *
+ * @since      0.11.0
+ *
+ * @version    $Id: Africa_47_Maseru.php 4669 2011-05-25 20:53:42Z david $
+ */
+
+return array (
+  'types' => 
+  array (
+    0 => 
+    array (
+      'rawOffset' => 7200,
+      'dstOffset' => 0,
+      'name' => 'SAST',
+    ),
+    1 => 
+    array (
+      'rawOffset' => 7200,
+      'dstOffset' => 3600,
+      'name' => 'SAST',
+    ),
+  ),
+  'rules' => 
+  array (
+    0 => 
+    array (
+      'time' => -2109289800,
+      'type' => 0,
+    ),
+    1 => 
+    array (
+      'time' => -829526400,
+      'type' => 1,
+    ),
+    2 => 
+    array (
+      'time' => -813805200,
+      'type' => 0,
+    ),
+  ),
+  'finalRule' => 
+  array (
+    'type' => 'static',
+    'name' => 'SAST',
+    'offset' => 7200,
+    'startYear' => 1945,
+  ),
+  'source' => 'africa',
+  'version' => '8.31',
+  'name' => 'Africa/Maseru',
+);
+
+?>
